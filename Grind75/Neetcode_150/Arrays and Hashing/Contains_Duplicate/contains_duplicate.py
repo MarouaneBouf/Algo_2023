0dc3b3eef4452c03785ab4c3ded1eb6782c1d95d
MarouaneBouf <104838272+MarouaneBouf@users.noreply.github.com>
2022-08-15 12:18:56 +0100

    Create contains_duplicate.py

---
+++ Grind75/Neetcode_150/Arrays and Hashing/Contains_Duplicate/contains_duplicate.py
@@ -0,0 +1,13 @@
+# 70 / 70 test cases passed.
+# Status: Accepted
+# Runtime: 592 ms
+# Memory Usage: 26 MB
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+        for i in nums :
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False
