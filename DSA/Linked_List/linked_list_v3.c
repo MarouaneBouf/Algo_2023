5aab344cc5712bdf5dea9100950ec261b5acd89d
MarouaneBouf <marouanebouf3@gmail.com>
2022-10-21 18:30:11 +0100

    Return from functions instead of void

---
+++ DSA/Linked_List/linked_list_v3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+}node;
+
+node *add_element(node *list, int data)
+{
+    node *new_node = (node *)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = list;
+    list = new_node;
+    return list;
+}
+
+void display(node *list)
+{
+    node *temp = list;
+    while(temp)
+    {
+        fprintf(stdout,"%i\n",temp->data);
+        temp = temp->next;
+    }
+}
+
+node *add_end(node *list, int data)
+{
+    node *new_node = (node *)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = NULL;
+    node *temp = list;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = new_node;
+    return list;
+}
+
+int main(void)
+{
+    node *list = NULL;
+    list = add_element(list,100);
+    list = add_element(list,120);
+    list = add_end(list, 1832);
+    display(list);
+}
