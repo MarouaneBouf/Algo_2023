d158f73889ac2911df5d5423a439a80f882e487f
MarouaneBouf <marouanebouf3@gmail.com>
2022-11-07 00:28:38 +0100

    Update Linked_list folder

---
+++ DSA/Linked_List/pile.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct point
+{
+    int x;
+    int y;
+    int z;
+    struct point *suivant;
+}point;
+
+
+point *ajouter_pile(point *tete,int x,int y, int z)
+{
+    point *new_point = malloc(sizeof(point));
+    new_point->x = x;
+    new_point->y = y;
+    new_point->z = z;
+    new_point->suivant = NULL;
+    new_point->suivant = tete;
+    tete = new_point;
+    return tete;
+}    
+
+point *supprimer_et_afficher(point *tete)
+{
+    point *temp = tete;
+    printf("x = (%i, %i, %i)\n",temp->x,temp->y,temp->z);
+    tete = tete->suivant;
+    free(temp);
+    return tete;
+}
+
+void affichage(point *tete)
+{
+    point *temp = tete;
+    int k = 0;
+    while(temp)
+    {
+        printf("x%i = (%i, %i, %i)\n",k++,temp->x,temp->y,temp->z);
+        temp = temp->suivant;
+    }
+}
+
+int main(void)
+{
+    point *pile = NULL;
+    pile = ajouter_pile(pile, 10, 12, 19);
+    pile = ajouter_pile(pile, 16, 68, 95);
+    pile = ajouter_pile(pile, 3213, 21, 6586);
+    pile = supprimer_et_afficher(pile);
+    printf("------------------\n");
+    affichage(pile);
+}
---
+++ DSA/Linked_List/point.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct point
+{
+    int x;
+    int y;
+    int z;
+    struct point *point_suivant;
+}pt;
+
+pt* ajout_debut(pt *origine, int x, int y, int z)
+{
+    pt *point_ = malloc(sizeof(pt));
+    point_->x = x;
+    point_->y = y;
+    point_->z = z;
+    point_->point_suivant = origine;
+    origine = point_;
+    return origine;
+}
+
+pt* ajout_fin(pt *origine, int x, int y, int z)
+{
+    pt *point_ = malloc(sizeof(pt));
+    point_->x = x;
+    point_->y = y;
+    point_->z = z;
+    point_->point_suivant = NULL;
+    pt *temp = origine;
+    while(temp->point_suivant != NULL)
+    {
+        temp = temp->point_suivant;
+    }
+    temp->point_suivant = point_;
+    return origine;
+}
+
+pt* suppression_debut(pt *origine)
+{
+    origine = origine->point_suivant;
+    return origine;
+}
+
+pt* suppression_fin(pt *origine)
+{
+    pt *temp = origine;
+    while (temp->point_suivant->point_suivant != NULL)
+    {
+        temp = temp->point_suivant;
+    }
+    temp->point_suivant = NULL;
+    return origine;
+}
+
+void affichage(pt *origine)
+{
+    pt *temp = origine;
+    int k = 0;
+    while(temp)
+    {
+        printf("x%i = (%i, %i, %i)\n",k++,temp->x,temp->y,temp->z);
+        temp = temp->point_suivant;
+    }
+}
+
+
+int main(void)
+{
+    pt *origine = NULL;
+
+    origine = ajout_fin(origine,1,236,380);
+
+    affichage(origine);
+
+}
